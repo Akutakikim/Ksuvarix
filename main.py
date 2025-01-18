ea06fc0ca163c5e831434216fcb5d267ae84d173
Akutakikim <akutakikim@gmail.com>
2025-01-18 23:47:27 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from flask import Flask, request, jsonify
 from pymongo import MongoClient
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler, filters, ContextTypes
-
+# variables 
+BOT_TOKEN = "7751315434:AAHN7vB3uet6YtHBjlf3agyAdtda0IjYxx4"
+MONGO_URI = "mongodb+srv://Zafinet:<Akik20f20varb04>@zafinet.wftow.mongodb.net/?retryWrites=true&w=majority&appName=Zafinet"
+ADMIN_USER_ID = 5912828707  # Replace with your Telegram user ID (Admin)
 
 # Logging Configuration
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
